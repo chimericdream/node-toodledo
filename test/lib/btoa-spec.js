3eb5d5e11ab580d889a03ddf2c85c01b73d15edf
Bill Parrott <bill@chimericdream.com>
2016-08-06 23:09:11 -0500

    start adding tests

---
+++ test/lib/btoa-spec.js
@@ -0,0 +1,21 @@
+const btoa = require('../../src/lib/btoa');
+
+const chai = require('chai');
+
+const should = chai.should;
+const expect = chai.expect;
+const assert = chai.assert;
+
+const strings = [
+    ['this is a string', 'dGhpcyBpcyBhIHN0cmluZw=='],
+    ['this is another string', 'dGhpcyBpcyBhbm90aGVyIHN0cmluZw=='],
+    ['hi, my name is string', 'aGksIG15IG5hbWUgaXMgc3RyaW5n']
+];
+
+describe('The btoa (base64 encoding) function', () => {
+    it('should base64 encode typical strings', () => {
+        strings.forEach((testCase) => {
+            expect(btoa(testCase[0])).to.equal(testCase[1]);
+        });
+    });
+});
